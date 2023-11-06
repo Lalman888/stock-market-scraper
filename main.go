de73e049fc3f72e80b3e7d34bca835adca606626
lalman888 <chauhanlakshay074@gmail.com>
2023-11-07 00:13:26 +0530

    fix

--- main.go
+++ main.go
@@ -6,24 +6,55 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/gocolly/colly"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Stock struct {
-	Company, Price, Change string
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
 
-var stocks []Stock
-var mu sync.Mutex
+type Stock struct {
+	Company, Price, Change string
+	ScrapingDate           time.Time // Add the date when the data was scraped
+}
+
+var (
+	stocks     []Stock
+	stocksLock sync.Mutex
+)
 
 func main() {
-	app := fiber.New()
+	loadEnv()
 
+	// Create a ticker that runs every 24 hours
+	ticker := time.NewTicker(24 * time.Hour)
+
+	go func() {
+		for {
+			scrapeAndInsertData()
+			<-ticker.C // Wait for the ticker to trigger
+		}
+	}()
+
+	// Start the Fiber server
+	app := setupFiber()
+	err := app.Listen(":3000")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func scrapeAndInsertData() {
 	ticker := []string{
 		"MSFT",
 		"IBM",
@@ -61,19 +92,16 @@ func main() {
 		stock.Company = e.ChildText("h1")
 		stock.Price = e.ChildText("fin-streamer[data-field='regularMarketPrice']")
 		stock.Change = e.ChildText("fin-streamer[data-field='regularMarketChangePercent']")
+		stock.ScrapingDate = time.Now() // Record the scraping date
 
-		mu.Lock()
+		stocksLock.Lock()
 		stocks = append(stocks, stock)
-		mu.Unlock()
+		stocksLock.Unlock()
 	})
 
 	for _, t := range ticker {
 		c.Visit("https://finance.yahoo.com/quote/" + t + "/")
 	}
-
-	app.Get("/stocks", func(c *fiber.Ctx) error {
-		return c.JSON(stocks)
-	})
 
 	// MongoDB connection options
 	uri := os.Getenv("MONGODB_URI")
@@ -92,21 +120,40 @@ func main() {
 
 	// Insert the scraped stock data into MongoDB
 	var bsonStocks []interface{}
+	stocksLock.Lock()
 	for _, s := range stocks {
 		bsonStock, _ := bson.Marshal(s)
 		var unmarshaledStock interface{}
 		_ = bson.Unmarshal(bsonStock, &unmarshaledStock)
 		bsonStocks = append(bsonStocks, unmarshaledStock)
 	}
+	stocks = []Stock{} // Clear the stocks slice
+	stocksLock.Unlock()
 
 	_, err = collection.InsertMany(context.Background(), bsonStocks)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Server is running...")
-	err = app.Listen(":3000")
+	fmt.Println("Scraping and data insertion completed.")
+
+	// Start the Fiber server after scraping and data insertion
+	app := setupFiber()
+	err = app.Listen(":3001")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+}
+
+func setupFiber() *fiber.App {
+	app := fiber.New()
+
+	app.Get("/stocks", func(c *fiber.Ctx) error {
+		stocksLock.Lock()
+		defer stocksLock.Unlock()
+		return c.JSON(stocks)
+	})
+
+	fmt.Println("Fiber server is ready to serve.")
+	return app
 }
